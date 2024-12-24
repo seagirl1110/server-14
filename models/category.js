3989cdc2f585450e37ff7ba66ad9250f69ba82e6
Marina Elizarova <seagirl1110@gmail.com>
2024-12-24 20:44:21 +0300

    feat: add models Category and Product

---
+++ models/category.js
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose';
+
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+});
+
+const Category = model('Category', categorySchema);
+
+export default Category;
