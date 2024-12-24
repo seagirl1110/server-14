fce1082242f1625b3772585fed0f5ca5d3a12834
Marina Elizarova <seagirl1110@gmail.com>
2024-12-24 21:47:31 +0300

    feat: add create categories and products, get products

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import Category from './models/category.js';
+import Product from './models/product.js';
 
 const app = express();
 app.use(express.json());
@@ -23,6 +25,62 @@ mongoose
 
 app.get('/', (req, res) => {
   res.status(200).send('Server running');
+});
+
+app.post('/categories', async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      res.status(400).json({ message: 'Name is required' });
+    }
+
+    const category = new Category({ name });
+    await category.save();
+
+    res
+      .status(201)
+      .json({ message: `Category ${name} was successfully created` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Error when creating category', error: error.message });
+  }
+});
+
+app.post('/products', async (req, res) => {
+  try {
+    const { name, price, category } = req.body;
+
+    if (!name || !price || !category) {
+      res
+        .status(400)
+        .json({ message: 'Name, price and category are required' });
+    }
+
+    const product = new Product({ name, price, category });
+    await product.save();
+
+    res
+      .status(201)
+      .json({ message: `Product was successfully created`, product });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Error when creating product', error: error.message });
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find().populate('category');
+
+    res.status(200).json({ message: 'All products:', products });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Error when getting products', error: error.message });
+  }
 });
 
 app.listen(port, () => {
